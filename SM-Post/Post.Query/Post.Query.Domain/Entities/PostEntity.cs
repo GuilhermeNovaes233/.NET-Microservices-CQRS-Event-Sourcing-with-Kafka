4d4f858e6606f6de14a31985ed8e6cdfd3653920
Guilherme Novaes da silva <TRI005@einstein.br>
2023-03-14 16:44:00 -0300

    Criando entitidades

---
+++ SM-Post/Post.Query/Post.Query.Domain/Entities/PostEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Post.Query.Domain.Entities
+{
+	[Table("Post")]
+	public class PostEntity
+	{
+		public PostEntity()
+		{
+			Comments = new List<CommentEntity>();
+		}
+
+		[Key]
+		public Guid PostId { get; set; }
+		public string Author { get; set; }
+		public DateTime DatePosted { get; set; }
+		public string Message { get; set; }
+		public int Likes { get; set; }
+		public virtual ICollection<CommentEntity> Comments { get; set; } 
+	}
+}
---
+++ SM-Post/Post.Query/Post.Query.Domain/Repositories/ICommentRepoistory.cs
@@ -0,0 +1,12 @@
+﻿using Post.Query.Domain.Entities;
+
+namespace Post.Query.Domain.Repositories
+{
+	public interface ICommentRepoistory
+	{
+		Task CreateAsync(CommentEntity comment);
+		Task UpdateAsync(CommentEntity comment);
+		Task<CommentEntity> GetByIdAsync(Guid commentId);
+		Task DeleteAsync(Guid commentId);
+	}
+}

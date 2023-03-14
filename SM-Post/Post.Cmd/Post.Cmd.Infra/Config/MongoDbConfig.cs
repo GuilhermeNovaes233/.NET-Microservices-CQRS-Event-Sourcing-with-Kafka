77432b7fd0ab6356d359d033e811615ce82c2ef1
Guilherme Novaes da silva <TRI005@einstein.br>
2023-03-14 14:38:44 -0300

    Criando repositorio mongo

---
+++ SM-Post/Post.Cmd/Post.Cmd.Infra/Config/MongoDbConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Post.Cmd.Infra.Config
+{
+	public class MongoDbConfig
+	{
+		public string ConnectionString { get; set; }
+		public string Database { get; set; }
+		public string Collection { get; set; }
+	}
+}
